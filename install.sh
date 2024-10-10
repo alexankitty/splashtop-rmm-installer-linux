240dfba5bd2b19c63b7e33a54a3c308ac6edf422
Alexandra <45508320+alexankitty@users.noreply.github.com>
2024-10-09 19:22:24 -0600

    Clean up after ourselves

--- install.sh
+++ install.sh
@@ -3,4 +3,5 @@ rmm_client="https://packagesstore.blob.core.windows.net/installers/RemoteTools/R
 wineboot -u
 curl -o /tmp/RMM_Client.exe $rmm_client
 wine /tmp/RMM_Client.exe
+rm /tmp/RMM_Client.exe
 xdg-mime default wine-protocol-st-rmm.desktop x-scheme-handler/st-rmm
